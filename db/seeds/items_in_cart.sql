4e04d86502abbb06d967c77b2988bf89f543ef75
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-30 11:41:08 -0500

    database for storing items in cart

---
+++ db/seeds/items_in_cart.sql
@@ -0,0 +1,2 @@
+INSERT INTO items (user_id, menu_id, menu_name, menu_img_url, quantity)
+VALUES (1, 2, 'Sandwitch', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-subway-sub-1521220946.jpg?resize=980:*', 2);
