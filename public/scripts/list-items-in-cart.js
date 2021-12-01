21d204d9dd94955ba8a80db5c3989f29a6b52ddd
Bosi Chen <chenbosi.bosi@gmail.com>
2021-11-30 20:12:23 -0500

    merge and modification

--- public/scripts/list-items-in-cart.js
+++ public/scripts/list-items-in-cart.js
@@ -5,7 +5,6 @@ $(() => {
     $('#cart-item-container').append(`
         <article class="item-article" >
           <div class="checkbox"><input type="checkbox" class="item"></div>
-          <div class="img"><img src="${menu.menu_img_url}" height="70px" width="auto"></div>
           <div class="name"><b>${menu.name}</b></div>
           <div class="quantity">${menu.quantity}</div>
           <div class="price">$${menu.price}</div>
