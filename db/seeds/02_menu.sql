8a15105d9f7968b95c967615fbe2294600c25ca4
Sagal Afrah <afrahsagal@gmail.com>
2021-12-01 01:32:15 -0500

    changed URLS to edit photos for right aspect ratio

--- db/seeds/02_menu.sql
+++ db/seeds/02_menu.sql
@@ -1,12 +1,12 @@
 INSERT INTO menu (name, components, price, type, img)
 VALUES
-  ('Milky Way Latte', 'Milk, Espresso, Caramel syrup', 5, 'drinks', https://images.unsplash.com/photo-1593443320739-77f74939d0da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80),
-  ('Galaxy Matcha', 'Matcha Powder, Milk, Classic Syrup', 5, 'drinks', https://images.unsplash.com/photo-1619745445488-9c9cc94a4b14?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80),
-  ('Strawberry Neptune Refresher', 'Strawberry Juice, Black Tea, Classic Syrup', 5, 'drinks', https://images.unsplash.com/photo-1596343540266-130b3dbb1158?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80),
-  ('Blue Moon', 'Juniper tea', 5, 'drinks', https://www.ohhowcivilized.com/wp-content/uploads/2019/08/0819-butterfly-pea-flower-tea-latte-6.jpg),
-  ('Flat White', 'Espresso, Milk', 5, 'drinks', https://media.istockphoto.com/photos/white-cup-of-hot-cappuccino-art-coffee-on-wooden-table-picture-id542700652?b=1&k=20&m=542700652&s=170667a&w=0&h=d97Fajf4rbX6n1L9TyQwuoOrp4X4JXjPgKYxbyPiJio=),
-  ('Cosmo Brownie', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', https://images.unsplash.com/photo-1588539543889-20cc7ce4df55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80),
-  ('Moon Pies', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', https://images.unsplash.com/photo-1560180474-e8563fd75bab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80),
-  ('Macademia Disk', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', https://images.unsplash.com/photo-1504624182560-38adb60ff7a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80),
-  ('Pesto, Chicken and Mozzerella Rockets', 'Pesto, Chicken, Mozzerella', 3.5, 'food', https://images.unsplash.com/photo-1603903631889-b5f3ba4d5b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80),
-  ('Potato and Cheese Croquettes', 'Cheese, Potato, Flour, Egg', 3.5, 'food', https://www.thespruceeats.com/thmb/Anjbta921-dpdrJ3iMgwijR7PcQ=/3600x3600/smart/filters:no_upscale()/turkey-and-mashed-potato-croquettes-3061895-hero-01-e4f0afde158c474ca45d80a2081a2948.jpg)
+  ('Milky Way Latte', 'Milk, Espresso, Caramel syrup', 5, 'drinks', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/milkyway.png?raw=true'),
+  ('Galaxy Matcha', 'Matcha Powder, Milk, Classic Syrup', 5, 'drinks', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/galaxymatcha.png?raw=true'),
+  ('Strawberry Neptune Refresher', 'Strawberry Juice, Black Tea, Classic Syrup', 5, 'drinks', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/strawberryrefresher.png?raw=true'),
+  ('Blue Moon', 'Juniper tea', 5, 'drinks', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/bluemoon.png?raw=true'),
+  ('Flat White', 'Espresso, Milk', 5, 'drinks', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/flatwhite.png?raw=true'),
+  ('Cosmo Brownie', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/cosmobrownies.png?raw=true'),
+  ('Moon Pies', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/moonpies.png?raw=true'),
+  ('Macademia Disk', 'Chocolate, Eggs, Flour, Milk, Sugar', 3.5, 'food', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/macademiadisk.png?raw=true'),
+  ('Pesto, Chicken and Mozzerella Rockets', 'Pesto, Chicken, Mozzerella', 3.5, 'food', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/chicken.png?raw=true'),
+  ('Potato and Cheese Croquettes', 'Cheese, Potato, Flour, Egg', 3.5, 'food', 'https://github.com/Zeinaaaa/Midterm/blob/master/food_photos/potato.png?raw=true');
